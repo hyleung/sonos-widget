cdbb65dd528d0b7e6c4a8e16eb5064c7f5c6e088
Ho Yan Leung <hy.leung@gmail.com>
2015-12-26 21:37:35 -0800

    Initial Commit

---
+++ sonos-widget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sonos-widget
+//
+//  Created by Ho Yan Leung on 2015-12-26.
+//  Copyright © 2015 Ho Yan Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ sonos-widgetTests/sonos_widgetTests.swift
@@ -0,0 +1,36 @@
+//
+//  sonos_widgetTests.swift
+//  sonos-widgetTests
+//
+//  Created by Ho Yan Leung on 2015-12-26.
+//  Copyright © 2015 Ho Yan Leung. All rights reserved.
+//
+
+import XCTest
+@testable import sonos_widget
+
+class sonos_widgetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
