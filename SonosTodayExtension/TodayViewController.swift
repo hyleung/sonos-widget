66c3c230b34300d1564990917092d66c475c4afa
Ho Yan Leung <hy.leung@gmail.com>
2016-02-23 06:50:58 -0800

    Use tableview for today extension

--- SonosTodayExtension/TodayViewController.swift
+++ SonosTodayExtension/TodayViewController.swift
@@ -9,15 +9,20 @@
 import UIKit
 import NotificationCenter
 import sonosclient
-class TodayViewController: UIViewController, NCWidgetProviding {
-        
+class TodayViewController: UITableViewController, NCWidgetProviding {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.rowHeight = 50.0
+        self.tableView.sectionFooterHeight = 5.0
         // Do any additional setup after loading the view from its nib.
         SonosDiscoveryClient.performDiscovery()
         .subscribeNext { (s) -> Void in
             print(s)
         }
+        updatePreferredContentSize()
+        print("preferred content size: \(preferredContentSize)")
     }
     
     override func didReceiveMemoryWarning() {
@@ -33,6 +38,27 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // If there's an update, use NCUpdateResult.NewData
 
         completionHandler(NCUpdateResult.NewData)
+    }
+    
+    func updatePreferredContentSize() {
+//        preferredContentSize = CGSizeMake(CGFloat(0), CGFloat(tableView(tableView, numberOfRowsInSection: 0)) * CGFloat(tableView.rowHeight) + tableView.sectionFooterHeight)
+                preferredContentSize = CGSizeMake(CGFloat(0), CGFloat(tableView(tableView, numberOfRowsInSection: 0)) * CGFloat(tableView.rowHeight) + tableView.sectionFooterHeight)
+    }
+
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        cell.textLabel?.textColor = UIColor.whiteColor()
+        cell.textLabel?.text = "Hello World"
+        return cell
     }
     
 }
